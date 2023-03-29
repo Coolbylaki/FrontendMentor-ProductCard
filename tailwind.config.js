a2a3c10185dca6450e7534eaaf7bcc8f34ad4552
Lazar Stojanović <lakisd98@gmail.com>
2023-03-29 07:44:27 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				// Primary
+				darkCyan: "hsl(158, 36%, 37%)",
+				cream: "hsl(30, 38%, 92%)",
+
+				// Neutral
+				veryDarkBlue: "hsl(212, 21%, 14%)",
+				darkGrayishBlue: "hsl(228, 12%, 48%)",
+			},
+		},
+	},
+	plugins: [],
+};
